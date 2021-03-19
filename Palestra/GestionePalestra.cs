5df0a59cc499b4556507ef5adec59c26d0bbc020
samuele cavallucci <cavalluccisamuele369@gmail.com>
2021-03-19 15:51:18 +0100

    getsione paletsra

--- Palestra/GestionePalestra.cs
+++ Palestra/GestionePalestra.cs
@@ -5,6 +5,13 @@ namespace Palestra
     [TestClass]
     public class GestionePalestra
     {
-     
+        int c = 3;
+        double risultato = 300;
+        double calcolo = GestionePalestra;
+    }
+    public void TestGestionePalestra()
+    int d = 1;
+    string risultato = " la migliore è quella da 3 mesi";
+
     }
 }
--- Tariffe/Gestione_Tariffe.cs
+++ Tariffe/Gestione_Tariffe.cs
@@ -4,5 +4,10 @@ namespace Tariffe
 {
     public class Gestione_Tariffe
     {
+        if (c > 3 && < 3)
+            {
+            double b = c * 99;
+        return b;
+            }
     }
 }
